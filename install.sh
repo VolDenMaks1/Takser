bedefd37c874d9b4148034a4e5aafce7e2b6b4f9
VolDenMaks1 <omletgamer100@gmail.com>
2025-02-12 20:39:38 +0200

    Add C version of Takser (from scratch) and remove Python implementation;

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp takser.py /usr/local/bin/takser
-
-chmod +x /usr/local/bin/takser
-
-echo "The installation is complete. Now you can run 'takser' from anywhere."
--- takser.c
+++ takser.c
@@ -1,52 +1,63 @@
-// Task tracker "Takser" C verion
 #include <stdio.h>
-#include <json-c/json.h>
+#include <stdlib.h>
+#include <string.h>
+#include <cjson/cJSON.h> //Why is this showing an error?
+
+// Maint task:
+//     [x] Read the JSON file
+//     [] Write the JSON file
+
+typedef struct
+{
+    char text[1000];
+    int status; // 0: not done, 1: in progress, 2: done
+    int id;
+} Task;
+
+char * read_json(const char *filename)
+{
+
+    FILE *file = fopen(filename, "r"); // Opening file
+
+    if (!file)
+    { // Cheking if file is opened
+        fprintf(stderr, "Error: Unable to open file %s\n", filename);
+        return NULL;
+    }
+
+    fseek(file, 0, SEEK_END);  // Seeking to the end of the file
+    long length = ftell(file); // Getting file length
+    rewind(file);              // Going back to start point
+
+    char *content = (char *)malloc(length + 1); // Allocating memory for content    (what does ths do?)
+
+    if (!content)
+    { // Checking if memory is allocated
+        printf("Error: Unable to allocate memory\n");
+        return NULL;
+    }
+
+    fread(content, 1, length, file); // Reading file content
+    content[length] = '\0';          // Adding null character at the end of the content
+    fclose(file);                    // Closing file
+    return content;
+} // Summary: This function reads the content of the file and returns it as a string (I dont know how it works I stole it from ChatGPT)
 
 int main(int argc, char *argv[])
 {
-
-    FILE *fp;
-
-    char buffer[1024];
-
-    struct tasks *tasks;
-    struct tasks *text;
-    struct tasks *status;
-    struct tasks *id;
-
-    fp = fopen("tasks.json", "r");
-    fread(buffer, 1024, 1, fp);
-    fclose(fp);
-
-    tasks = json_tokener_parse(buffer);
-
-    json_object_object_get_ex(tasks, "text", &text);
-    json_object_object_get_ex(tasks, "text", &status);
-    json_object_object_get_ex(tasks, "text", &id);
-
-    if (argc < 2) {
-        list_tasks();
-        return 0;
-    }
-    else if (strcmp(argv[1], "add") == 0) 
-    {
-        add_task();
-    }
-    else if (strcmp(argv[1], "rm") == 0) 
-    {
-        remove_task();
-    }
-    else if (strcmp(argv[1], "d") == 0) 
-    {
-        done_task();
-    }
-    else if (strcmp(argv[1], "ip") == 0) 
-    {
-        in_progress_task();
-    }
-    else {
-        printf("Unknown command: %s\n", argv[1]);
-    }
-
-}
+    //TEST CODE
     
+    char *filename = "tasks.json";
+    char *content = read_json(filename);
+
+    if(!content)
+    {
+        return 1;
+    }
+
+    printf("%s\n", content);
+
+    free(content);
+}
+
+//Ths project is to hard for me. But i will still try to do it.
